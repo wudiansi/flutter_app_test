9fcbce1466929e2a67c921f6a2ffd6d01e201aab
wuds <wuds@cume.cc>
2020-06-17 18:29:26 +0800

    update lesson_group_page.dart

---
+++ lib/fast_key.md
@@ -0,0 +1,10 @@
+TOP1：查看最近打开的文件：command + E
+TOP2：万能的搜索：双击shift
+TOP3：查看当前类结构：command + fn + f12
+TOP4：重命名：fn+shift+f6
+TOP5：查看源码：将光标放到要查看源码的类名或方法名上，长按command 然后的点击
+TOP6：查看类的子类：选中要查看的类，然后：command + B 或 option + command + B
+TOP7：快速创建Widget：在dart文件中输入stf或stl出现提示后按回车即可，如：https://coding.imooc.com/learn/questiondetail/113157.html
+TOP8：将代码更新到模拟器上：选中模拟器然后 command + R
+TOP9：导入类的快捷键：将光标放在要导入类的上面，然后按 option + enter
+TOP10：快速创建构造函数：在类文件中通过command + N 然后在弹出的面板中选择constructor
---
+++ lib/lesson_group_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_color_plugin/flutter_color_plugin.dart';
+
+void main() => runApp(LessonGroupPage());
+
+// rename 快捷键shift + F6
+class LessonGroupPage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = TextStyle(fontSize: 20);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lesson group page'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.share), onPressed: () {},)
+          ],
+        ),
+//        drawer: new MyDrawer(),
+        body: Container(
+          decoration: BoxDecoration(color: Colors.white),
+          alignment: Alignment.center,
+          child: Column(
+            children: <Widget>[
+              Text(
+                'page test',
+                style: textStyle,
+              ),
+              Icon(
+                Icons.android,
+                size: 40,
+                color: Colors.green,
+              ),
+              CloseButton(),
+              BackButton(),
+              Chip(
+                avatar: Icon(Icons.people),
+                label: Text('peoples'),
+//                padding: EdgeInsets.all(10),
+              ),
+              Divider(
+//                无法设置分割线高度
+                height: 10,
+                indent: 10,
+                color: Colors.black,
+              ),
+              Card(
+                elevation: 5,
+                color: Colors.blue,
+                shadowColor: Colors.blue,
+                margin: EdgeInsets.all(10),
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text('This is a Card'),
+                ),
+
+              ),
+              AlertDialog(
+                title: Text('Dialog title'),
+                content: Text('Dialog content'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
