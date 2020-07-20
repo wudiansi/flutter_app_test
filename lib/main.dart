4a5bffe3a4d2bd069c7dd2fa2c8dd4ca60818ec8
wuds <wuds@cume.cc>
2020-07-20 17:50:16 +0800

    update           6-7 animatedWidget简化重构

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,24 @@ void main() {
   runApp(LogoApp());
 }
 
+class AnimationLogo extends AnimatedWidget {
+  AnimationLogo({Key key, Animation<double> animation})
+    : super(key: key, listenable: animation); // listenable animation
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return new Center(
+      child: new Container(
+        margin: new EdgeInsets.symmetric(vertical: 10.0),
+        height: animation.value,
+        width: animation.value,
+        child: new FlutterLogo(),
+      ),
+    );
+  }
+  
+}
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
@@ -27,45 +45,47 @@ class _LogoAppState extends State with SingleTickerProviderStateMixin {
     super.initState();
     // SingleTickerProviderStateMixin vsync 可隐藏时不动画
     controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+        new AnimationController(vsync: this, duration: const Duration(seconds: 2));
     //  为动画添加listener .. 表示返回值 等效于animation.XXX
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
-      ..addListener(() {
-        //  获取变化中的值
-        setState(() {
-          animationValue = animation.value;
-        });
-      })
-      ..addStatusListener((AnimationStatus state) {
-        setState(() {
-          animationState = state;
-        });
-      });
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+//      ..addListener(() {
+//        //  获取变化中的值
+//        setState(() {
+//          animationValue = animation.value;
+//        });
+//      })
+//      ..addStatusListener((AnimationStatus state) {
+//        setState(() {
+//          animationState = state;
+//        });
+//      });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          GestureDetector(
-            // 点击按钮 点击初始化 然后触发
-            onTap: (){
-              controller.reset();
-              controller.forward();
-            },
-            child: Text('start', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 40),),
-          ),
-          Text('State: ' + animationState.toString(), textDirection: TextDirection.ltr,),
-          Text('Value: ' + animationValue.toString(), textDirection: TextDirection.ltr,),
-          Container(
-            width: animation.value,
-            height: animation.value,
-            child: FlutterLogo(),
-          )
-        ],
-      ),
-    );
+    return new AnimationLogo(animation: animation);
+//    return Container(
+//      child: Column(
+//        children: <Widget>[
+//          GestureDetector(
+//            // 点击按钮 点击初始化 然后触发
+//            onTap: (){
+//              controller.reset();
+//              controller.forward();
+//            },
+//            child: Text('start', textDirection: TextDirection.ltr, style: TextStyle(fontSize: 40),),
+//          ),
+//          Text('State: ' + animationState.toString(), textDirection: TextDirection.ltr,),
+//          Text('Value: ' + animationValue.toString(), textDirection: TextDirection.ltr,),
+//          Container(
+//            width: animation.value,
+//            height: animation.value,
+//            child: FlutterLogo(),
+//          )
+//        ],
+//      ),
+//    );
   }
 
   @override
