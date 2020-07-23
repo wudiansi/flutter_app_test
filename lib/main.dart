250fdc0bc5106b190d0684c1acb2cc62d9d6fbea
wuds <wuds@cume.cc>
2020-07-23 15:25:29 +0800

    update           6-10 hero Animation createRectTween

--- lib/main.dart
+++ lib/main.dart
@@ -3,45 +3,38 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart' show timeDilation; // 动画执行速度
 
-class PhotoHero extends StatelessWidget {
-  const PhotoHero({Key key, this.photo, this.onTap, this.width})
-      : super(key: key);
+class Photo extends StatelessWidget {
+  const Photo({Key key, this.photo, this.color, this.onTap}) : super(key: key);
 
   final String photo;
   final VoidCallback onTap;
-  final double width;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      child: Hero(
-          tag: photo, // 通过tag标识
-          child: Material(
-            color: Theme.of(context).primaryColor.withOpacity(0.25),
-//            InkWell 墨水池
-            child: InkWell(
-              onTap: onTap,
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints size) {
-                  return Image.network(
-                    photo,
-                    fit: BoxFit.contain,
-                  );
-                },
-              ),
-            ),
-          )),
+    return Material(
+      color: Theme.of(context)
+          .primaryColor
+          .withOpacity(0.25), //            InkWell 墨水池
+      child: InkWell(
+        onTap: onTap,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints size) {
+            return Image.network(
+              photo,
+              fit: BoxFit.contain,
+            );
+          },
+        ),
+      ),
     );
   }
 }
 
 class RadialExpansion extends StatelessWidget {
-  RadialExpansion({
-    Key key,
-    this.maxRadius,
-    this.child
-  }) : clipRectSize = 2.0 * (maxRadius / math.sqrt2), super();
+  RadialExpansion({Key key, this.maxRadius, this.child})
+      : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
+        super(key: key);
 
   final double maxRadius;
   final clipRectSize;
@@ -67,18 +60,22 @@ class RadialExpansion extends StatelessWidget {
 class RadialExpansionDemo extends StatelessWidget {
   static const double kMinRadius = 32.0;
   static const double kMaxRadius = 128.0;
-  static const opacityCurve = const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
+  static const opacityCurve =
+      const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
 
-  static RectTween _createRectTween(Rect begin, Rect end){
+  static RectTween _createRectTween(Rect begin, Rect end) {
+    //    方形到圆形的辅助类
     return MaterialRectArcTween(begin: begin, end: end);
   }
+
 //  创建页面
-  static Widget _buildPage(BuildContext context, String imageName, String description) {
+  static Widget _buildPage(
+      BuildContext context, String imageName, String description) {
     return Container(
-      color: Theme.of(context).canvasColor,// 主题canvasColor
+      color: Theme.of(context).canvasColor, // 主题canvasColor
       child: Center(
         child: Card(
-          elevation: 8.0,// 阴影
+          elevation: 8.0, // 阴影
           child: Column(
             mainAxisSize: MainAxisSize.max,
             children: [
@@ -86,17 +83,23 @@ class RadialExpansionDemo extends StatelessWidget {
                 width: kMaxRadius * 2.0,
                 height: kMaxRadius * 2.0,
                 child: Hero(
+                    // createRectTween 描述形状变化
                     createRectTween: _createRectTween,
                     tag: imageName,
                     child: RadialExpansion(
-                    maxRadius: kMaxRadius,
-                    child: PhotoHero(
-                      photo: imageName,
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                  ),
-                )),
+                      maxRadius: kMaxRadius,
+                      child: Photo(
+                        photo: imageName,
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    )),
+              ),
+              Text(
+                description,
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 3.0,
               )
             ],
           ),
@@ -105,37 +108,37 @@ class RadialExpansionDemo extends StatelessWidget {
     );
   }
 
-  Widget _buildHero(BuildContext context, String imageName, String description){
+  Widget _buildHero(
+      BuildContext context, String imageName, String description) {
     return Container(
       width: kMinRadius * 2,
       height: kMinRadius * 2,
       child: Hero(
-          createRectTween: _createRectTween,
+          createRectTween: _createRectTween,// 镜像动画与普通动画的区别
           tag: imageName,
           child: RadialExpansion(
             maxRadius: kMaxRadius,
-            child: PhotoHero(
+            child: Photo(
               photo: imageName,
               onTap: () {
-                Navigator.of(context).push(
-                  PageRouteBuilder<void>(
-                    pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation){
-                      return AnimatedBuilder(
-                        animation: animation,
-                        builder: (BuildContext context, Widget child) {
-                          return Opacity(
-                              opacity: opacityCurve.transform(animation.value),
-                            child: _buildPage(context, imageName, description),
-                          );
-                        },
+                //  PageRouteBuilder生成一次性路由
+                Navigator.of(context).push(PageRouteBuilder<void>(pageBuilder:
+                    (BuildContext context, Animation<double> animation,
+                        Animation<double> secondaryAnimation) {
+                  // AnimatedBuilder 回调改变当前页面opacity值
+                  return AnimatedBuilder(
+                    animation: animation,
+                    builder: (BuildContext context, Widget child) {
+                      return Opacity(
+                        opacity: opacityCurve.transform(animation.value),
+                        child: _buildPage(context, imageName, description),
                       );
-                    }
-                  )
-                );
+                    },
+                  );
+                }));
               },
             ),
-          )
-      ),
+          )),
     );
   }
 
@@ -144,66 +147,30 @@ class RadialExpansionDemo extends StatelessWidget {
     timeDilation = 10.0;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Radial Hero Animation'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(32.0),
-        alignment: FractionalOffset.bottomLeft,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,// 横向排列平分
-          children: [
-            _buildHero(context, 'https://raw.githubusercontent.com/flutter/website/master/examples/_animation/radial_hero_animation/images/chair-alpha.png', 'Chair'),
-            _buildHero(context, 'https://raw.githubusercontent.com/flutter/website/master/examples/_animation/radial_hero_animation/images/binoculars-alpha.png', 'Binoculars'),
-            _buildHero(context, 'https://raw.githubusercontent.com/flutter/website/master/examples/_animation/radial_hero_animation/images/beachball-alpha.png', 'Beach ball'),
-          ],
+        appBar: AppBar(
+          title: const Text('Radial Hero Animation'),
         ),
-      )
-    );
-  }
-}
-
-
-
-class HeroAnimation extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    timeDilation = 10.0;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Basic Hero Animation'),
-      ),
-      body: Center(
-        child: PhotoHero(
-          photo: 'https://www.cume.cc/img/partner/avatar_white.png',
-          width: 300.0,
-          onTap: () {
-//            of(context) 上下文
-            Navigator.of(context)
-                .push(MaterialPageRoute<void>(builder: (BuildContext context) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: const Text('Flippers Page'),
-                ),
-                body: Container(
-                  color: Colors.lightGreenAccent,
-                  padding: const EdgeInsets.all(16.0),
-                  alignment: Alignment.topLeft,
-                  child: PhotoHero(
-                    photo: 'https://www.cume.cc/img/partner/avatar_white.png',
-                    width: 100.0,
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ),
-              );
-            }));
-          },
-        ),
-      ),
-    );
+        body: Container(
+          padding: const EdgeInsets.all(32.0),
+          alignment: FractionalOffset.bottomLeft,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // 横向排列平分
+            children: [
+              _buildHero(
+                  context,
+                  'https://www.cume.cc/img/small_banner_3.jpg',
+                  'Chair'),
+              _buildHero(
+                  context,
+                  'https://www.cume.cc/img/small_banner_2.jpg',
+                  'Binoculars'),
+              _buildHero(
+                  context,
+                  'https://www.cume.cc/img/small_banner_4.jpg',
+                  'Beach ball'),
+            ],
+          ),
+        ));
   }
 }
 
